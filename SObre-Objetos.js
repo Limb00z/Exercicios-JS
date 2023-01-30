abcde820e0446d88c604ee66775d3b5da35c0cf3
Ruan Reis <ruancosme23@gmail.com>
2023-01-30 03:50:35 -0300

    Explicando sobre a criação!

--- SObre-Objetos.js
+++ SObre-Objetos.js
@@ -1,8 +1,26 @@
 /*
+    OBJETOS
+        Assim como os arrays, também são estruturas de dados, capazes de armazenar e organizar outros dados.
+        Dados são organizados através de propriedades.
+        Associação chave:valor ---
+            Chave é o nome da propriedade;
+            valor é o valor que esta propriedade vai receber.
+        Também podem armazenar internamente qualquer outro tipo
+            incluive arrays e outros objetos.
 
+    CRIAÇÃO
+        Para podermos criar um objeto, é parecido com array, mas tem algumas particularidades. ex:
 
+        let clientes = {
+            nome: "Ruan",
+            idade: 28,
+            pgtoEmDIa: true
+        }
 
-
+        se dermos um console.log(clientes); veremos todas as informações acima. 
+        E se por acaso eu quiser apenas ver o nome? ex:
+        console.log(clientes.nome); // natualmente ele vai retornar todos as propriedades nome e seus respectivos valores.
+    
 
 
 
