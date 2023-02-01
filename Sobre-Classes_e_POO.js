ac704e9518850f4d9c0d71df01d89b55640ded82
Ruan Reis <ruancosme23@gmail.com>
2023-01-31 23:51:58 -0300

    Inicio sobre classes

--- Sobre-Classes_e_POO.js
+++ Sobre-Classes_e_POO.js
@@ -1,1 +1,18 @@
-// POO ( ou OOP)
+// POO ( ou OOP) e Classes
+
+/*
+    **Todo objeto é criado a partir de uma classe.
+    **A classe é uum molde utilizado pra criar um objeto.
+        dentro da classe nós conseguimos defifnir atributos e métodos que um objeto receberá.
+    ex: 
+        Class Nave {
+        };  // a classe é criada assim. 
+
+        let maquinaVoadora = new Nave(); // para chamarmos a classe, temos que usar os parenteses. 
+
+    **Conseguimos também alem de aceitar todos os tipos que já conhecemos, (string, number, bolean e etc), Com as classes também podemos criar uma especie de tipo prórpio, digamos, derivado. 
+
+
+
+
+*/
