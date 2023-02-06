6286787d38b9da81320288656967c3a5929fea73
Ruan Reis <ruancosme23@gmail.com>
2023-02-05 23:33:07 -0300

    Capturei os tipos  e blindagem e munição

---
+++ 07/script.js
@@ -0,0 +1,12 @@
+const f_tipoMilitar = document.querySelector("#f_tipoMilitar");
+const f_tipoNormal = document.querySelector("#f_tipoNormal");
+
+const f_blindagem = document.querySelector("#f_blindagem");
+const f_municao = document.querySelector("#f_municao");
+
+f_tipoMilitar.addEventListener("click", (evt) => {
+    console.log("cliquei")
+})
+f_tipoNormal.addEventListener("click", (evt) => {
+    console.log("cliquei")
+})
