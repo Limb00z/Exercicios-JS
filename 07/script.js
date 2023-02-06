981f09c6a9b4cdb1d2e140cfbb635f65470bc83a
Ruan Reis <ruancosme23@gmail.com>
2023-02-05 23:36:22 -0300

    funções ao clicar em militar ou normal

--- 07/script.js
+++ 07/script.js
@@ -5,8 +5,15 @@ const f_blindagem = document.querySelector("#f_blindagem");
 const f_municao = document.querySelector("#f_municao");
 
 f_tipoMilitar.addEventListener("click", (evt) => {
-    console.log("cliquei")
-})
+    console.log("cliquei tipo militar");
+    f_blindagem.removeAttribute("disabled")
+    f_municao.removeAttribute("disabled")
+});
+
 f_tipoNormal.addEventListener("click", (evt) => {
-    console.log("cliquei")
-})
+    console.log("cliquei tipo normal");
+    f_blindagem.value = 0;
+    f_municao.value = 0;
+    f_blindagem.setAttribute("disabled", "disabled");
+    f_municao.setAttribute("disabled", "disabled");
+});
