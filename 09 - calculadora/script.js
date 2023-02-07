eb718e1e399d3b83e7d647505b0897ea6d8f3d5e
Ruan Reis <ruancosme23@gmail.com>
2023-02-07 16:37:18 -0300

    Criada a função de liga e desliga

--- 09 - calculadora/script.js
+++ 09 - calculadora/script.js
@@ -10,8 +10,8 @@ console.log(teclasRes);
 const display = document.querySelector(".display");
 console.log(display);
 
-const ton = document.querySelector("#ton");
-console.log(ton);
+const tpower = document.querySelector("#tpower");
+console.log(tpower);
 
 const tlimpar = document.querySelector("#tlimpar");
 console.log(tlimpar);
@@ -30,9 +30,25 @@ teclasNum.forEach((elemento) => {
 teclasOp.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => { 
         display.innerHTML += evento.target.innerHTML
-    })
-})
+    });
+});
 
 tlimpar.addEventListener("click", (evento) => {
-    display.innerHTML = "0";
-})
+   
+});
+
+tpower.addEventListener("click", () => {
+    if (tpower.innerHTML == "ON") {
+        tpower.innerHTML = "OFF";
+        display.innerHTML = "DESLIGADA"
+        tpower.style.background = "#e44";
+        tpower.style.borderColor = "#800"
+        display.style.background = "rgb(158, 158, 158)";
+    } else {
+        tpower.innerHTML = "ON";
+        tpower.style.background = "rgb(56, 212, 108)";
+        tpower.style.borderColor = "rgb(3, 71, 26)"
+        display.style.background = "#8a8";
+        display.innerHTML = "0";
+    };
+});
