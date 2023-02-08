d061c95bbb4e46eef79e5e5b607fb8373dffef8e
Ruan Reis <ruancosme23@gmail.com>
2023-02-07 22:17:06 -0300

    script para funcionar apenas quando estiver ON

--- 09 - calculadora/script.js
+++ 09 - calculadora/script.js
@@ -4,8 +4,8 @@ console.log(teclasNum);
 const teclasOp = [...document.querySelectorAll(".op")];
 console.log(teclasOp);
 
-const teclasRes = document.querySelector(".res");
-console.log(teclasRes);
+const teclaRes = document.querySelector(".res");
+console.log(teclaRes);
 
 const display = document.querySelector(".display");
 console.log(display);
@@ -16,26 +16,56 @@ console.log(tpower);
 const tlimpar = document.querySelector("#tlimpar");
 console.log(tlimpar);
 
+let sinal = false;
+
+let decimal = false;
 
 
 
 
 teclasNum.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
-        display.innerHTML += evento.target.innerHTML
+        sinal = false;
+        if (tpower.innerHTML == "ON") {
+            if (evento.target.innerHTML == ",") {
+                if (!decimal) {
+                    decimal = true;
+                    if (display)
+                        display.innerHTML += evento.target.innerHTML
+                }
+            
+            } else {
+                display.innerHTML += evento.target.innerHTML
+            }
+        }
     });
 });
 
 
 teclasOp.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => { 
-        display.innerHTML += evento.target.innerHTML
+        if (tpower.innerHTML == "ON"){
+
+            if (!sinal) {
+                sinal = true;
+                if (evento.target.innerHTML == "x") {
+                    display.innerHTML += "*"
+                } else {
+                    display.innerHTML += evento.target.innerHTML
+                }
+            }
+        }
     });
 });
 
 tlimpar.addEventListener("click", (evento) => {
-   
+    if (tpower.innerHTML == "ON") {
+        display.innerHTML = ""
+    }
 });
+
+
+
 
 tpower.addEventListener("click", () => {
     if (tpower.innerHTML == "ON") {
@@ -49,6 +79,6 @@ tpower.addEventListener("click", () => {
         tpower.style.background = "rgb(56, 212, 108)";
         tpower.style.borderColor = "rgb(3, 71, 26)"
         display.style.background = "#8a8";
-        display.innerHTML = "0";
+        display.innerHTML = "";
     };
 });
