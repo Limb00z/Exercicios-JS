a9f3f2980a8c3e60ebdf8b5b16a6118d7c901ffe
Ruan Reis <ruancosme23@gmail.com>
2023-02-10 11:00:54 -0300

    Correção html e capturado os elementos

---
+++ 13 - bolinhas/script.js
@@ -0,0 +1,7 @@
+const palco = document.getElementById("palco");
+const num_objetos = document.getElementById("num_objetos");
+const txt_qtde = document.getElementById("txt_qtde");
+const btn_add = document.getElementById("btn_add");
+const btn_remover = document.getElementById('btn_remover')
+
+//console.log(palco,num_objetos,txt_qtde,btn_add,btn_remover);
