98097d4f0d84f2a4bd4b1024e23d671007231796
Ruan Reis <ruancosme23@gmail.com>
2023-02-10 10:10:03 -0300

    finalizado os movimentos dos ohos via JS

--- 12/script.js
+++ 12/script.js
@@ -7,4 +7,10 @@ window.addEventListener("mousemove", (evento) => {
     posX_mouse = evento.clientX;
     posY_mouse = evento.clientY;
     console.log(posX_mouse);;// para ver a posição do mouse.
+
+    const rotacao = Math.atan2(posY_mouse,posX_mouse) * 180 / Math.PI
+
+    olhos.forEach((o) => {
+       o.style.transform = "rotate("+rotacao+"deg)" 
+    });
 });
