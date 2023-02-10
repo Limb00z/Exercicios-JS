22180a2e2f300004ef93133110d9648de84c274a
Ruan Reis <ruancosme23@gmail.com>
2023-02-09 21:49:21 -0300

    Capturado todos os elemntos do horario

--- 11- relogio/script.js
+++ 11- relogio/script.js
@@ -2,12 +2,20 @@ const div_relogio = document.querySelector(".div_relogio");
 console.log(div_relogio);
 
 
-
 const relogio = () => {
-   const data = new Date();
- console.log(data);
+   
+const data = new Date();
+console.log(data);
 
-    let hora = data.getHours();
-    hora = hora < 10 ? "0"+hora : hora; //Operarador ternario. lembra?
-    console.log(hora);
+let hora = data.getHours();
+hora = hora < 10 ? "0"+hora : hora; //Operarador ternario. lembra?
+console.log(hora);
+
+let min = data.getMinutes();
+min = min < 10 ? "0" + min : min;
+console.log(min);
+
+let seg = data.getSeconds();
+seg = seg < 10 ? "0" + seg : seg;
+console.log(seg);
 }
