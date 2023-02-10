a828a45dbd105c4311f4e4e2124a554c8e45f801
Ruan Reis <ruancosme23@gmail.com>
2023-02-09 21:27:37 -0300

    metodos descritos

--- Sobe-Manipulação_de_Datas.js
+++ Sobe-Manipulação_de_Datas.js
@@ -1,6 +1,25 @@
 /*
 
 
+getDate() = Dia do mês
+getDay() = Dia da Semana (número)
+getFullYear() = Ano com 4 dígitos
+getHours() = Horas
+getMilliseconds() = Milisegundos
+getMinutes() = Minutos
+getMonth() = Mês
+getSeconds() = Segundos
+getTime() = Timestamp (milisegundos desde 1 de Janeiro de 1970, 00:00:00 UTC)
+Date.now() = Timestamp (milisegundos desde 1 de Janeiro de 1970, 00:00:00 UTC)
+getTimezoneOffset() = Timezone da localidade
+setDate() = Define um dia do mês para a data
+setMonth() = Define um mês para a data
+setFullYear() = Define um ano para a data
+setHours() = Define horas
+setMinutes() = Define Minutos
+setSeconds() = Define Segundos
+setMilliseconds() = Define milisegundos
+toDateString()
 
 
 
