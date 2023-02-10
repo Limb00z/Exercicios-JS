55febcbb6666907a2b8a8911d2fe6bc370698fd2
Ruan Reis <ruancosme23@gmail.com>
2023-02-10 11:43:55 -0300

    criando uma class Bola

--- 13 - bolinhas/script.js
+++ 13 - bolinhas/script.js
@@ -12,6 +12,17 @@ let alturaPalco = palco.offsetHeight;
 let bolas = [];
 let numBola = 0;
 
+class Bola {
+    constructor() {
+        this.tam = Math.floor((Math.random() * 15)) + 10;
+        this.r = Math.floor(Math.random() * 255);
+        this.g = Math.floor(Math.random() * 255);
+        this.b = Math.floor(Math.random() * 255);
+        this.px = Math.floor(Math.random() * larguraPalco - this.tam);
+        this.py = Math.floor(Math.random() * alturaPalco - this.tam);
+    }
+}
+
 window.addEventListener("resize", (evento) => {
     //console.log('teste');
     larguraPalco = palco.offsetWidth;
