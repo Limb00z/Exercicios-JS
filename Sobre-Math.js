ece8985a6c15238138bc9ba38fd03588195c1650
Ruan Reis <ruancosme23@gmail.com>
2023-02-10 09:41:10 -0300

    comentários

--- Sobre-Math.js
+++ Sobre-Math.js
@@ -1,5 +1,12 @@
 /*
 
+    MATH --  É uma biblioteca básica de matemática, com metódos e propriedades matematicos. Ela não precisa ser instanciada numa variavel, ela é estática. faz auto-referencia. Mas não impede de fazer...
 
+    //para gerar de 0 a 9
+    Math.floor(Math.random()*10);
+    
+    para gerar de 1 a 10, eliminando o 0.
+    Math.floor(Math.random()*10) +1;
+    
 
 */
