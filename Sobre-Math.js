1fc6bbc0f024273ebf857a990429ec8a9bbe8166
Ruan Reis <ruancosme23@gmail.com>
2023-02-10 10:18:59 -0300

    finalização dos estudos

--- Sobre-Math.js
+++ Sobre-Math.js
@@ -7,6 +7,36 @@
     
     para gerar de 1 a 10, eliminando o 0.
     Math.floor(Math.random()*10) +1;
-    
+
+
+    Constante	Descrição
+    Math.E	Retorna o número Euler
+    Math.PI	Retorna o número PI
+    Math.SQRT2	Retorna a raiz quadrada de 2
+    Math.SQRT1_2	Retorna a raiz quadrada de ½
+    Math.LN2	Retorna o logaritmo natural de 2
+    Math.LN10	Retorna o logaritmo natual de 10
+    Math.LOG2E	Retorna base 2 do logaritmo de E
+    Math.LOG10E	Retorna base decimal do logaritmo de E
+
+    Método	Descrição
+    abs(x)	Retorna o valor absoluto de X
+    acos(x)	Retorna o arco cosseno de X
+    asin(x)	Retorna o arco seno de X
+    atan(x)	Retorna o arco tangente de X como um valor numérico entre PI/2 e PI/2 radiano
+    atan2(y,x)	Retorna o arco tangente do quociente dos argumentos y e x
+    ceil(x)	Retorna o valor de X arredondado para cima
+    cos(x)	Retorna o cosseno de x em radianos
+    exp(x)	Retorna o valor de Ex
+    floor(x)	Retorna o valor de X arredondado para baixo
+    log(x)	Retorna o logaritmo natural (base E) de x
+    max(x,y,z,…,n)	Retorna o maior valor dos argumentos
+    min(x,y,z,…,n)	Retorna o menor valor dos argumentos
+    pow(x,y)	Retorna o valor de x elevado a y
+    random()	Retorna um número aleatório entre 0 e 1
+    round(x)	Retorna o inteiro mais próximo, arredonda para cima ou para baixo
+    sin(x)	Retorna o seno de x em radianos
+    sqrt(x)	Retorna a raiz quadrada de x
+    tan(x)	Retorna a tangente do ângulo x
 
 */
