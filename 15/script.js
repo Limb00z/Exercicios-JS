a4844931deca550d1f86db2cab4b3ae074b46044
Ruan Reis <ruancosme23@gmail.com>
2023-02-13 00:26:28 -0300

    eventos de click associados.

--- 15/script.js
+++ 15/script.js
@@ -1,3 +1,18 @@
 const btn_alert = document.getElementById("btn_alert");
 const btn_confirm = document.getElementById("btn_confirm");
 const btn_prompt = document.getElementById("btn_prompt");
+
+
+btn_alert.addEventListener("click", (evento) => {
+    
+});
+
+
+btn_confirm.addEventListener("click", (evento) => {
+    
+});
+
+
+btn_prompt.addEventListener("click", (evento) => {
+    
+});
