c2dbc8b9c5c7c3e771dc57a473859b006099032d
Ruan Reis <ruancosme23@gmail.com>
2023-02-15 23:58:08 -0300

    exemplificando

--- Sobre-Modulos.js
+++ Sobre-Modulos.js
@@ -1,7 +1,13 @@
 /*
     Módulos -- Os módulos permitem que blocos de código como funções, classes e constantes em arquivos separados possam ser importados quando necessário, facilitando o processo de manutenção e reusabilidade de um projeto JavaScript.
 
-    
+        COMO FAZER?
+            Depois que nós separarmos os arquivos as funções  e o que mais quisermos separar, usamos um export{função/array/class}-- O que você quiser exportar;
+
+            E como utlizar? Usamos a a palavra import no começo do arquivo JS principal. 
+            import {função/array/class} from "localização/pasta"
+            ex; 
+                import {calcular} from "./JS/calcular.js"
 
 
 
