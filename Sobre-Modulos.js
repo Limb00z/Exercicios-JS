2d5b8610ac6fb9916e88620e04fd943656ce639b
Ruan Reis <ruancosme23@gmail.com>
2023-02-15 23:53:06 -0300

    explicação simples do módulo JS

---
+++ Sobre-Modulos.js
@@ -0,0 +1,11 @@
+/*
+    Módulos -- Os módulos permitem que blocos de código como funções, classes e constantes em arquivos separados possam ser importados quando necessário, facilitando o processo de manutenção e reusabilidade de um projeto JavaScript.
+
+    
+
+
+
+
+
+
+*/
