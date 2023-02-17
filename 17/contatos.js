6ebc6796af6ec26f931d53cbf5ab7b3fa811930c
Ruan Reis <ruancosme23@gmail.com>
2023-02-17 10:32:47 -0300

    finalizado

--- 17/contatos.js
+++ 17/contatos.js
@@ -2,10 +2,10 @@ import { contatos } from "./bancoContatos.js";
 
 let contato = {
     getTodosContatos: function () {
-        return contatos
+        return contatos;
     },
     getContato: function (i_cont) {
-        return contatos[i_cont]
+        return contatos[i_cont];
     },
     addContato: function (novoContato, destinoDOM) {
         const cont = {
@@ -13,26 +13,42 @@ let contato = {
             telefone: novoContato.telefone,
             email: novoContato.email
         }
-        
-        contatos.push(cont)
-        
-        const div = document.createElement('div');
-        div.setAttribute("class", "contato");
+        contatos.push(cont);
 
-        const p_nome = document.createElement("p");
-        p_nome.innerHTML = novoContato.nome;
-        const p_telefone = document.createElement("p");
-        p_telefone.innerHTML = novoContato.telefone;
-        const p_email = document.createElement("p");
-        p_email.innerHTML = novoContato.email;
+        destinoDOM.innerHTML = '';
 
-        div.appendChild(p_nome);
-        div.appendChild(p_telefone);
-        div.appendChild(p_email);
+        contatos.forEach((c, indice) => {
+            
+            const btn_remover = document.createElement('button')
+            btn_remover.setAttribute('id', 'btn_' + indice);
 
-        destinoDOM.appendChild(div)
+            const div = document.createElement('div');
+            div.setAttribute('class', 'contato');
 
+            btn_remover.innerHTML = 'Remover';
+            btn_remover.addEventListener('click', (evt) => {
+                destinoDOM.innerHTML = '';
+                contatos.splice(indice, 1);
+                contato.addContato(contatos, destinoDOM);
+            });
+
+            if (c.nome != undefined) {
+                const p_nome = document.createElement('p');
+                p_nome.innerHTML = c.nome;
+                const p_telefone = document.createElement('p');
+                p_telefone.innerHTML = c.telefone;
+                const p_email = document.createElement('p');
+                p_email.innerHTML = c.email;
+                
+                div.appendChild(p_nome);
+                div.appendChild(p_telefone);
+                div.appendChild(p_email);
+                div.appendChild(btn_remover);
+                destinoDOM.appendChild(div);
+            } else {
+                contatos.pop();
+            };
+        });
     }
 };
-
 export default contato
