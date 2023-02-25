f6742a3157f4ee1c77ab5ef7f46974538adb6719
Ruan Reis <ruancosme23@gmail.com>
2023-02-24 23:37:22 -0300

    explciação e metódo length

--- Sobre-String.js
+++ Sobre-String.js
@@ -1,5 +1,18 @@
 /*
-
+     As strings são textos, e usamos para atribuição de tipo às varaiveis. 
+     Nós podemos clasifcar uma variavel como string apenas usando aspas duplas entre o valor.
+     ex:
+        let nome = "Ruan"; Se fizermos um typeof(nome), o reultado erá string.
+    E por ser uma string, nós temos diversos metodos para podermos usar a seu favor. 
+    Alguns dos mais comuins são:
+        .length
+            A propriedade length serve para nos informar o tamanho de uma string.
+            Para testar a propriedade length, vamos usar a string Estudo, que retornará o tamanho 6.
+                const palavra="Estudo";
+                console.log(palavra.length); //6
+        
+        
+        
 
 
 
