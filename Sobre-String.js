692e7745f4b073e0aca6a9844f6828b162e27462
Ruan Reis <ruancosme23@gmail.com>
2023-02-24 23:46:28 -0300

    obs em indexOf

--- Sobre-String.js
+++ Sobre-String.js
@@ -27,6 +27,7 @@
             Por exemplo:
                 const palavra="Estudo"
                 console.log(palavra.indexOf("o")) //5 
+            obs: fique atento caso o caractere que se busca na string seja encontrado em mais de uma posição, pois será retornada somente a primeira ocorrência.
         
 
 
