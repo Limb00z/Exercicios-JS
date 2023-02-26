bc13a9830a0f3b9e97dc55635877ce1e91ca8973
Ruan Reis <ruancosme23@gmail.com>
2023-02-26 13:04:13 -0300

    Elementos capturados

---
+++ 19 -getBoundingClientRect( )/script.js
@@ -0,0 +1,6 @@
+const q1 = document.getElementById("q1");
+const q2 = document.getElementById("q2");
+const posx = document.getElementById("posx");
+const posy = document.getElementById("posy");
+const largura = document.getElementById("largura");
+const altura = document.getElementById("altura");
