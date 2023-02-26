2ce9dda0cfbc779d9db909085492557bbe3c27c5
Ruan Reis <ruancosme23@gmail.com>
2023-02-25 22:55:46 -0300

    arquivos iniciais ok. objetos capturados

---
+++ 18 - Animações/script.js
@@ -0,0 +1,6 @@
+const carro = document.getElementById("carro");
+const btn_esquerda = document.getElementById("btn_esquerda");
+const btn_direita = document.getElementById("btn_direita");
+
+
+//console.log(carro,btn_direita,btn_esquerda);
