4b61c81abef0e23feb6048bf44d10f10476c0395
Ruan Reis <ruancosme23@gmail.com>
2023-02-26 00:28:01 -0300

    finalizando a movimentação e uso das setas tbm

--- 18 - Animações/script.js
+++ 18 - Animações/script.js
@@ -1,5 +1,6 @@
 const carro = document.getElementById("carro");
 const btn_esquerda = document.getElementById("btn_esquerda");
+const btn_parar = document.getElementById("btn_parar");
 const btn_direita = document.getElementById("btn_direita");
 
 
@@ -10,14 +11,51 @@ const init = () => {
     carro.style.left = "0px";
 };
 
+let anima = null;
+
+const move = (dir) => {
+    carro.style.left = parseInt(carro.style.left) + (10*dir) + "px";
+};
+
+btn_parar.addEventListener("click", () => {
+    clearInterval(anima);
+})
+
 btn_esquerda.addEventListener("click", () => {
-    let pos = parseInt(carro.style.left);
-    pos -= 10;
-    carro.style.left =`${pos}px`;
+    clearInterval(anima);
+    anima = setInterval(move, 50, -1);
 });
 
 btn_direita.addEventListener("click", () => {
-    carro.style.left = parseInt(carro.style.left) + 10 + "px";
+    clearInterval(anima);
+    anima = setInterval(move, 50, 1);
+})
+
+document.addEventListener("keydown", (evento) => {
+    let key = evento.key;
+
+    switch (evento.key) {
+        case "ArrowLeft":
+            clearInterval(anima);
+            anima = setInterval(move, 50, -1);
+            break;
+        
+        case "ArrowRight":
+            clearInterval(anima);
+            anima = setInterval(move, 50, 1);
+            break;
+        
+        case "ArrowDown":
+            clearInterval(anima);
+            break;
+        
+        case "ArrowUp":
+            clearInterval(anima);
+            break;
+        
+        default:
+            break;
+    }
 })
 
 window.onload = init;
