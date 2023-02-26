6888938020e4526350623daf1b1fd8603a84d43d
Ruan Reis <ruancosme23@gmail.com>
2023-02-25 23:40:55 -0300

    frente e pra trpas finalizado, o básico!

--- 18 - Animações/script.js
+++ 18 - Animações/script.js
@@ -4,3 +4,20 @@ const btn_direita = document.getElementById("btn_direita");
 
 
 //console.log(carro,btn_direita,btn_esquerda);
+
+const init = () => {
+    carro.style.position = "relative";
+    carro.style.left = "0px";
+};
+
+btn_esquerda.addEventListener("click", () => {
+    let pos = parseInt(carro.style.left);
+    pos -= 10;
+    carro.style.left =`${pos}px`;
+});
+
+btn_direita.addEventListener("click", () => {
+    carro.style.left = parseInt(carro.style.left) + 10 + "px";
+})
+
+window.onload = init;
