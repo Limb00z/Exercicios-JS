6ab98e60716cffcc4f4672ce8c0336d044ace3e1
Ruan Reis <ruancosme23@gmail.com>
2023-02-26 13:19:34 -0300

    pegando informações

--- 19 -getBoundingClientRect( )/script.js
+++ 19 -getBoundingClientRect( )/script.js
@@ -4,3 +4,11 @@ const posx = document.getElementById("posx");
 const posy = document.getElementById("posy");
 const largura = document.getElementById("largura");
 const altura = document.getElementById("altura");
+
+let DOMRect_q1 = q1.getBoundingClientRect();
+
+posx.innerHTML = `posx: ${DOMRect_q1.x}`;
+posy.innerHTML = `posy: ${DOMRect_q1.y}`;
+largura.innerHTML = `largura: ${DOMRect_q1.width}`;
+altura.innerHTML = `altura: ${DOMRect_q1.height}`;
+
