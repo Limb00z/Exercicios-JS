595d6793ea244d0bc58b485f1fe365c1550a00fc
Ruan Reis <ruancosme23@gmail.com>
2023-03-02 18:39:54 -0300

    tratamento para envio, POST

--- 24- FETCH/script.js
+++ 24- FETCH/script.js
@@ -2,6 +2,8 @@ const p_temp = document.getElementById("p_temp");
 const p_nivel = document.getElementById("p_nivel");
 const p_press = document.getElementById("p_press");
 
+const btn = document.getElementById("btn");
+
 
 const endpoint = "https://testenodejs.limb00z.repl.co/";
 
@@ -28,3 +30,28 @@ const endpoint = "https://testenodejs.limb00z.repl.co/";
 // };
 
 // let intervalo = setInterval(atualizar,3000)
+
+
+let dados = {
+    nome: "Ruan",
+    curso: "CFBCursos",
+    curso: "JS"
+};
+
+let cabeçalho = {
+    method: "POST",
+    body: JSON.stringify(dados)
+};
+
+const gravardados = () =>{
+    const endpoint = "https://testenodejs.limb00z.repl.co/";
+    fetch(endpoint, cabeçalho)
+        .then(res => res.json())
+        .then(ret => {
+        console.log(ret)
+    })
+}
+
+btn.addEventListener("click", () => {
+    gravardados();
+})
